0cf56dc3be5cdce9c842d089aca0525cb42c0637
SINHEESEOP <shfurwnd97@gmail.com>
2025-02-28 17:11:33 +0900

    25-02-28 타일장식물

---
+++ src/baekjoon/silver/타일장식물.java
@@ -0,0 +1,24 @@
+package baekjoon.silver;
+
+import java.util.Scanner;
+
+public class 타일장식물 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        long[] fib = new long[N + 2];  // F(N+1)까지 필요하므로 N+2 크기
+        fib[1] = 1;
+        fib[2] = 1;
+
+        for (int i = 3; i <= N + 1; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        long perimeter = 2 * (fib[N] + fib[N + 1]);
+        System.out.println(perimeter);
+    }
+
+}
---
+++ src/gold/하노이의탑.java
@@ -0,0 +1,10 @@
+package gold;
+
+public class 하노이의탑 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
