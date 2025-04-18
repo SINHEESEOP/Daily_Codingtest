b2356a42bc9f9df0fbe8c020ac9530984c5822e7
SINHEESEOP <shfurwnd97@gmail.com>
2025-04-18 23:45:56 +0900

    25-04-18 포도주 시식(DP)

---
+++ src/baekjoon/silver/note.java
@@ -0,0 +1,15 @@
+package baekjoon.silver;
+
+// 연습중
+public class note {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i : arr) {
+            System.out.println(arr[i-1]);
+        }
+
+    }
+}
