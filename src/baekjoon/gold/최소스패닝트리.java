49f6c1a5268662d6f3a4d7cf1b64c85119d4ddf0
SINHEESEOP <shfurwnd97@gmail.com>
2025-05-15 21:44:54 +0900

    25-05-15 최소스패닝트리

---
+++ src/baekjoon/gold/최소스패닝트리.java
@@ -0,0 +1,71 @@
+package baekjoon.gold;
+
+import java.io.*;
+import java.util.*;
+
+public class 최소스패닝트리 {
+
+    static int V, E;
+    static int[] parent;
+
+    static class Edge implements Comparable<Edge> {
+        int u, v, w;
+
+        Edge(int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) parent[b] = a;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        parent = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            parent[i] = i;
+        }
+
+        List<Edge> edges = new ArrayList<>(E);
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(u, v, w));
+        }
+        Collections.sort(edges);
+
+        long mstWeight = 0;
+        int count = 0;
+        for (Edge e : edges) {
+            if (find(e.u) != find(e.v)) {
+                union(e.u, e.v);
+                mstWeight += e.w;
+                if (++count == V - 1) break;
+            }
+        }
+
+        System.out.println(mstWeight);
+    }
+}
+
