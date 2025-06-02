7e965584dd47225ce86e67deeb6075d3ce33af23
SINHEESEOP <shfurwnd97@gmail.com>
2025-06-02 20:32:58 +0900

    25-06-02 콜라문제 (프로그래머스)

---
+++ src/programmers/level1/옹알이.java
@@ -0,0 +1,25 @@
+package programmers.level1;
+
+public class 옹알이 {
+
+    static final String[] keywords = {"aya", "ye", "woo", "ma"};
+
+    public static void main(String[] args) {
+
+        int result = solution(new String[] {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"});
+        System.out.println(result);
+    }
+
+
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        String[] keywords = {"aya", "ye", "woo", "ma"};
+
+        
+
+
+        return answer;
+    }
+
+}
+
