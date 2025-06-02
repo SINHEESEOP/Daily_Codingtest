02d50b346467a04356a3d5cfd4eaa351a1f07b94
SINHEESEOP <shfurwnd97@gmail.com>
2025-06-02 20:33:16 +0900

    25-06-02 콜라문제 (프로그래머스)

---
+++ src/programmers/level1/콜라문제.java
@@ -0,0 +1,23 @@
+package programmers.level1;
+
+public class 콜라문제 {
+
+    public static void main(String[] args) {
+
+        int result = solution(90, 89, 100);
+        System.out.println(result);
+
+    }
+
+    private static int solution(int a, int b, int n) {
+        int answer = 0;
+        while (n >= a) {
+            int tt = 0; // 주고 남은 콜라
+            tt += n % a;
+            answer += (n / a) * b; //서비스로 받은 콜라 누적
+            n = (n / a) * b + tt; // 남아있는 콜라
+        }
+        return answer;
+    }
+
+}
