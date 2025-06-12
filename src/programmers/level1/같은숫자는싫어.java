2cfc1588705af0ff7037945085d3a15caa7dd881
SINHEESEOP <shfurwnd97@gmail.com>
2025-06-12 21:02:15 +0900

    25-06-11 프로그래머스 문제3개

---
+++ src/programmers/level1/같은숫자는싫어.java
@@ -0,0 +1,34 @@
+package programmers.level1;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class 같은숫자는싫어 {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(Arrays.toString(solution(new int[]{1, 1, 3, 3, 0, 1, 1})));
+
+    }
+
+    private static int[] solution(int[] ints) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(ints[0]);
+        for (int i = 1; i < ints.length; i++) {
+            if (stack.peek() == ints[i]) continue;
+            stack.push(ints[i]);
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+
+}
---
+++ src/programmers/level1/행렬의덧셈.java
@@ -0,0 +1,23 @@
+package programmers.level1;
+
+import java.util.Arrays;
+
+public class 행렬의덧셈 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(solution(new int[][]{{1, 2}, {2, 3}}, new int[][]{{3, 4}, {5, 6}})));
+    }
+
+    private static int[][] solution(int[][] ints1, int[][] ints2) {
+
+
+        for (int i = 0; i < ints1.length; i++) {
+            for (int j = 0; j < ints1[i].length; j++) {
+                ints1[i][j] = ints1[i][j] + ints2[i][j];
+            }
+        }
+
+        return ints1;
+    }
+
+}
