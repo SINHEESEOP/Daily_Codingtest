cc141fee8e8bda986523311724c3b4ca5d659b77
SINHEESEOP <shfurwnd97@gmail.com>
2025-07-01 21:54:29 +0900

    25-07-01 나무조각

---
+++ src/baekjoon/bronze/나무조각.java
@@ -0,0 +1,38 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 나무조각 {
+
+    static int[] arr = new int[5];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int cnt = 0;
+        while (st.hasMoreTokens()) {
+            arr[cnt++] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    StringBuilder sb = new StringBuilder();
+                    for (int i1 : arr) {
+                        sb.append(i1).append(" ");
+                    }
+                    System.out.println(sb);
+                }
+            }
+        }
+
+    }
+}
