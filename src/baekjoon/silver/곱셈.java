de461f2b5e2d2703772f1b80b7a21e064ed4d184
SINHEESEOP <shfurwnd97@gmail.com>
2025-07-06 07:28:37 +0900

    25-07-06 곱셈

---
+++ src/baekjoon/silver/곱셈.java
@@ -0,0 +1,31 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 곱셈 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a =  Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        long c =  Integer.parseInt(st.nextToken());
+
+        System.out.println(recursion(a, b, c));
+    }
+
+
+    private static long recursion(long a, long b, long c) {
+        if (b == 1) return a % c;
+
+        long result = recursion(a, b/2, c);
+        if (b % 2 == 0) {
+            return (result * result) % c;
+        }
+        return  (result * result % c) * a % c;
+    }
+}
