996d1dfbead6ce211bcca01deb51ae3e7d04ae58
SINHEESEOP <shfurwnd97@gmail.com>
2025-07-09 15:16:58 +0900

    25-07-09 별찍기11

---
+++ src/baekjoon/gold/별찍기11.java
@@ -0,0 +1,46 @@
+package baekjoon.gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 별찍기11 {
+
+    static char[][] star;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int width = n * 2 - 1;
+        star = new char[n][width];
+
+        writerStar(n, width / 2, 0);
+        StringBuilder sb = new StringBuilder();
+        for (char[] chars : star) {
+            for (char aChar : chars) {
+                sb.append(aChar == 0 ? ' ' : aChar);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void writerStar(int n, int y, int x) {
+        if (n == 3) {
+            star[x][y] = '*';
+            star[x + 1][y - 1] = '*';
+            star[x + 1][y + 1] = '*';
+            for (int i = -2; i <= 2; i++) {
+                star[x + 2][y - i] = '*';
+            }
+            return;
+        }
+
+        int half = n / 2;
+
+        writerStar(half, y, x);
+        writerStar(half, y - half, x + half);
+        writerStar(half, y + half, x + half);
+    }
+
+}
