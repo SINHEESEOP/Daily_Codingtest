199dcc5519389105d520e60abda4fe0cc1035574
SINHEESEOP <shfurwnd97@gmail.com>
2025-07-14 08:51:08 +0900

    fix: 백준_브론즈2_Strfry

--- src/algorithms/simulation/백준_브론즈2_Strfry.java
+++ src/algorithms/simulation/백준_브론즈2_Strfry.java
@@ -1,15 +1,33 @@
 package algorithms.simulation;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class 백준_브론즈2_Strfry {
 
-    public static void main(String[] args) {
-        System.out.println(isStrfry("ring", "gnir"));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            String[] input = br.readLine().split(" ");
+            String a = input[0];
+            String b = input[1];
+
+            if (isStrfry(a, b)) {
+                System.out.println("Possible");
+            } else {
+                System.out.println("Impossible");
+            }
+        }
     }
 
     public static boolean isStrfry(String a, String b) {
         if (a.length() != b.length()) return false;
 
-        int[] count = new int[26]; // 알파벳 소문자 a~z
+        int[] count = new int[26];
 
         for (char ch : a.toCharArray()) count[ch - 'a']++;
         for (char ch : b.toCharArray()) count[ch - 'a']--;
@@ -20,6 +38,4 @@ public class 백준_브론즈2_Strfry {
 
         return true;
     }
-
-
 }
