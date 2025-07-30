d512c81b7752c62a63117829485b8b7fe3dbd378
SINHEESEOP <shfurwnd97@gmail.com>
2025-07-30 15:04:27 +0900

    feat : 백준_실버4_제로 Pass

--- src/datastructures/list/vector/stack/백준_실버4_제로.java
+++ src/datastructures/list/vector/stack/백준_실버4_제로.java
@@ -1,7 +1,34 @@
 package datastructures.list.vector.stack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class 백준_실버4_제로 {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Stack<Integer> stack = new Stack<>();
+    static int sum;
 
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            Integer k = Integer.parseInt(br.readLine());
+            if (k == 0) {
+                stack.pop();
+                continue;
+            }
+            stack.push(k);
+        }
+
+        for (Integer i : stack) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
 
 }
