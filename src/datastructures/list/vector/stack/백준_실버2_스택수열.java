945b911626f8fb1c3bb4b982a58d5539787132bb
SINHEESEOP <shfurwnd97@gmail.com>
2025-07-31 10:20:54 +0900

    feat : 백준_실버2_스택수열 start

---
+++ src/datastructures/list/vector/stack/백준_실버2_스택수열.java
@@ -0,0 +1,25 @@
+package datastructures.list.vector.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 백준_실버2_스택수열 {
+
+    static int N;
+    static int K;
+    static int sequence = 1;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        while (N != 0) {
+            
+
+        }
+    }
+
+}
