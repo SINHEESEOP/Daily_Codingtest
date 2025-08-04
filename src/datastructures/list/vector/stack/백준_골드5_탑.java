38d740bd4067e80301c7775d881ed96f9c3ae2c0
SINHEESEOP <shfurwnd97@gmail.com>
2025-08-04 17:51:31 +0900

    feat : 백준_실버4_괄호 retry

--- src/datastructures/list/vector/stack/백준_골드5_탑.java
+++ src/datastructures/list/vector/stack/백준_골드5_탑.java
@@ -4,6 +4,7 @@ public class 백준_골드5_탑 {
 
     public static void main(String[] args) {
 
+
     }
 
 }
--- src/datastructures/list/vector/stack/백준_실버4_괄호.java
+++ src/datastructures/list/vector/stack/백준_실버4_괄호.java
@@ -1,9 +1,35 @@
 package datastructures.list.vector.stack;
 
+import java.io.*;
+import java.util.*;
+
 public class 백준_실버4_괄호 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String s = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            boolean valid = true;
+
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    stack.push(c);
+                } else {
+                    if (stack.isEmpty()) {
+                        valid = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+
+            sb.append((valid && stack.isEmpty()) ? "YES" : "NO").append('\n');
+        }
+
+        System.out.print(sb);
     }
-
 }
