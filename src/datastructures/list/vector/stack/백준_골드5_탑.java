89a1915fda99c0e405cba0f183984e1efef9e25b
SINHEESEOP <shfurwnd97@gmail.com>
2025-08-07 12:58:04 +0900

    feat : 백준_실버2_쇠막대기 fail

--- src/datastructures/list/vector/stack/백준_골드5_탑.java
+++ src/datastructures/list/vector/stack/백준_골드5_탑.java
@@ -5,6 +5,7 @@ public class 백준_골드5_탑 {
     public static void main(String[] args) {
 
 
+
     }
 
 }
---
+++ src/datastructures/list/vector/stack/백준_실버2_쇠막대기.java
@@ -0,0 +1,40 @@
+package datastructures.list.vector.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 백준_실버2_쇠막대기 {
+
+    static int stick;
+    static int result;
+    static Stack<String> stack = new Stack<>();
+    // ()(((()())(())()))(())
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push("(");
+                stick++;
+                result++;
+            } else if (c == ')') {
+                if (stack.lastElement().equals("(")) {
+                    stick--;
+                    result--;
+                    result += stick;
+                    stack.pop();
+                } else {
+                    stack.pop();
+                    stick--;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+
+
+}
