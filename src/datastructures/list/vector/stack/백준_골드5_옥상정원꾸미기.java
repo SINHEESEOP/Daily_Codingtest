decb1e4505888d0eab49edf33835db2dfccdfa40
SINHEESEOP <shfurwnd97@gmail.com>
2025-08-28 17:03:11 +0900

    feat : 백준_골드5_옥상정원꾸미기 start

---
+++ src/datastructures/list/vector/stack/백준_골드5_옥상정원꾸미기.java
@@ -0,0 +1,9 @@
+package datastructures.list.vector.stack;
+
+public class 백준_골드5_옥상정원꾸미기 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/datastructures/list/vector/stack/백준_실버2_스택수열.java
+++ src/datastructures/list/vector/stack/백준_실버2_스택수열.java
@@ -1,47 +1,19 @@
 package datastructures.list.vector.stack;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 import java.util.Stack;
 
 public class 백준_실버2_스택수열 {
 
     static int N;
-    static int[] K;
     static int sequence = 1;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static Stack<Integer> stack = new Stack<>();
     static StringBuilder sb = new StringBuilder();
+    static Stack<Integer> stack = new Stack<>();
 
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        K = new int[N];
-        for (int i = 0; i < N; i++) {
-            K[i] = Integer.parseInt(br.readLine());
-        }
 
-        int idx = 0;
-        while (idx < N) {
+        // 다시 풀어보기
 
-            if (!stack.isEmpty() && K[idx] == stack.peek()) {
-                stack.pop();
-                sb.append("-\n");
-                idx++;
-                continue;
-            }
-
-            if (sequence > N) {
-                System.out.println("NO");
-                return;
-            }
-
-            stack.push(sequence++);
-            sb.append("+\n");
-        }
-
-        System.out.println(sb);
     }
 }
