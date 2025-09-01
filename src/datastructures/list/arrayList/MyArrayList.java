841c46e5fe68c4fdcff6073195881da9594a181c
SINHEESEOP <shfurwnd97@gmail.com>
2025-09-01 16:07:20 +0900

    feat : MyArrayList - get(), size(), add(E e), add(int index, E e) 구현완료

--- src/datastructures/list/arrayList/MyArrayList.java
+++ src/datastructures/list/arrayList/MyArrayList.java
@@ -2,6 +2,8 @@ package datastructures.list.arrayList;
 
 import datastructures.list.MyList;
 
+import java.util.Arrays;
+
 public class MyArrayList<E> implements MyList<E> {
 
     // 내부 필드
@@ -9,33 +11,88 @@ public class MyArrayList<E> implements MyList<E> {
     private int size;
 
     // 생성자
-    public MyArrayList() { }
-    public MyArrayList(int initialCapacity) { }
+    public MyArrayList() {
+        this.elementData = new Object[10]; // 디폴트 초기 용량 (자바 ArrayList도 기본 10)
+        this.size = 0;
+    }
+
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+        this.elementData = new Object[initialCapacity];
+        this.size = 0;
+    }
 
     // ------ 조회 ------
     @Override
-    public E get(int index) { return null; }
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        rangeCheck(index);
+        return (E) elementData[index];
+    }
+
     @Override
-    public int size() { return 0; }
+    public int size() {
+        return this.size;
+    }
 
     // ------ 삽입 ------
     @Override
-    public void add(E e) { }
+    public void add(E e) {
+        ensureCapacity(size + 1);
+        elementData[size++] = e;
+    }
+
     @Override
-    public void add(int index, E e) { }
+    public void add(int index, E e) {
+        rangeCheck(index);          // ← index == size 허용
+        ensureCapacity(size + 1);         // ← 필요 시만 확장
+
+        // 뒤로 한 칸 밀기: [index .. size-1] → [index+1 .. size]
+        int numMoved = size - index;
+        if (numMoved > 0) {
+            System.arraycopy(elementData, index, elementData, index + 1, numMoved);
+        }
+
+        elementData[index] = e;
+        size++;
+    }
 
     // ------ 수정 ------
     @Override
-    public E set(int index, E e) { return null; }
+    public E set(int index, E e) {
+        return null;
+    }
 
     // ------ 삭제 ------
     @Override
-    public E remove(int index) { return null; }
+    public E remove(int index) {
+        return null;
+    }
     // (clear는 default라 구현 생략 가능)
 
     // ------ 탐색 ------
     @Override
-    public int indexOf(Object o) { return -1; }
+    public int indexOf(Object o) {
+        return -1;
+    }
     // (contains도 default라 구현 생략 가능)
+
+
+    // ---- Utility Method ----
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (elementData.length < minCapacity) {
+            int newCap = (elementData.length == 0) ? 10 : elementData.length * 2;
+            if (newCap < minCapacity) newCap = minCapacity;
+            elementData = java.util.Arrays.copyOf(elementData, newCap);
+        }
+    }
 
 }
