723353882698c79ad0e4ee4e9941640d3c192984
SINHEESEOP <shfurwnd97@gmail.com>
2025-09-01 16:51:02 +0900

    feat : 백준_골드5_옥상정원꾸미기.java FAIL - 다음날 재도전 하기.

--- src/datastructures/list/vector/stack/백준_골드5_옥상정원꾸미기.java
+++ src/datastructures/list/vector/stack/백준_골드5_옥상정원꾸미기.java
@@ -1,9 +1,29 @@
 package datastructures.list.vector.stack;
 
+import java.io.*;
+import java.util.*;
+
 public class 백준_골드5_옥상정원꾸미기 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int h = Integer.parseInt(br.readLine());
+
+            while (!stack.isEmpty() && stack.peek() <= h) {
+                stack.pop();
+            }
+            ans += stack.size(); // 현재 건물에서 보이는 이전 건물 수
+            stack.push(h);
+        }
+
+        System.out.print(ans);
     }
-
 }
+
