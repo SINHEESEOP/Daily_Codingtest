fd53cc34bd1d5942db4e673d8bf85385327ff393
SINHEESEOP <shfurwnd97@gmail.com>
2025-09-02 13:24:23 +0900

    feat : 백준_골드5_옥상정원꾸미기.java PASS

--- src/datastructures/list/vector/stack/백준_골드5_옥상정원꾸미기.java
+++ src/datastructures/list/vector/stack/백준_골드5_옥상정원꾸미기.java
@@ -6,24 +6,18 @@ import java.util.*;
 public class 백준_골드5_옥상정원꾸미기 {
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-        Deque<Integer> stack = new ArrayDeque<>();
         long ans = 0;
-
+        int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             int h = Integer.parseInt(br.readLine());
-
-            while (!stack.isEmpty() && stack.peek() <= h) {
-                stack.pop();
-            }
-            ans += stack.size(); // 현재 건물에서 보이는 이전 건물 수
+            while (!stack.isEmpty() && stack.peek() <= h) stack.pop();
+            ans += stack.size();
             stack.push(h);
         }
-
-        System.out.print(ans);
+        System.out.println(ans);
     }
 }
 
