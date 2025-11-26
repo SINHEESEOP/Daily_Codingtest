d15fb77142b3e0f60a2c460d28c26faf957b9ac5
sinheeseop <shfurwnd97@gmail.com>
2025-11-26 16:09:25 +0900

    회사에서 시간 잠깐 떠서 만들어본 백터 자료구조

--- src/datastructures/list/vector/MyVector.java
+++ src/datastructures/list/vector/MyVector.java
@@ -5,7 +5,7 @@ import datastructures.list.MyList;
 /**
  * MyVector는 동적 배열 기반의 자료구조로,
  * 요소들을 순차적으로 저장하며 필요 시 자동으로 배열 크기를 늘릴 수 있습니다.
- *
+ * <p>
  * 주요 특징:
  * - 내부적으로 배열(Object[])을 사용하여 요소를 관리합니다.
  * - 요소의 추가(add), 조회(get), 수정(set), 삭제(remove) 등 기본 List 연산을 지원합니다.
@@ -13,7 +13,7 @@ import datastructures.list.MyList;
  * - 배열 기반이므로 인덱스 접근이 빠르며(랜덤 액세스), 삽입/삭제는 느릴 수 있습니다.
  * - Java 표준 Vector와 유사하나, 동기화(synchronized)는 기본 지원하지 않습니다.
  * - MyList 인터페이스를 구현하여 List 계열 자료구조와 호환됩니다.
- *
+ * <p>
  * 실질적으로 ArrayList와 유사하지만, 필요 시 Vector처럼 Stack 등에서 상속하여 사용할 수 있습니다.
  */
 public class MyVector<E> implements MyList<E> {
@@ -26,39 +26,93 @@ public class MyVector<E> implements MyList<E> {
 
     // 기본 생성자: 기본 용량으로 배열 초기화
     public MyVector() {
-
+        this.elementData = new Object[10];
+        this.size = 0;
     }
 
     // 초기 용량 지정 생성자
     public MyVector(int initialCapacity) {
-
+        if (initialCapacity <= 0) {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+        this.elementData = new Object[initialCapacity];
+        this.size = 0;
     }
 
     // 지정한 인덱스의 요소를 반환
     @Override
-    public E get(int index) { return null; }
+    public E get(int index) {
+        // 범위 검사
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        // E 타입으로 캐스팅하여 반환
+        @SuppressWarnings("unchecked")
+        E value = (E) elementData[index];
+        return value;
+    }
+
 
     // 저장된 요소의 개수를 반환
     @Override
-    public int size() { return 0; }
+    public int size() {
+        return this.size;
+    }
 
     // 맨 뒤에 요소 추가
     @Override
-    public void add(E e) { }
+    public void add(E e) {
+        if (e == null) throw new NullPointerException();
+        ensureCapacity();
+        elementData[size++] = e;
+    }
 
     // 지정한 위치에 요소 삽입
     @Override
-    public void add(int index, E e) { }
+    public void add(int index, E e) {
+        if (e == null) throw new NullPointerException();
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        ensureCapacity();
+
+        if (index == size) {
+            elementData[size++] = e;
+        } else {
+            for (int i = size - 1; i >= index; i--) {
+                elementData[i + 1] = elementData[i];
+            }
+            elementData[index] = e;
+        }
+    }
 
     // 지정한 위치의 요소를 새로운 값으로 교체하고, 이전 값을 반환
     @Override
-    public E set(int index, E e) { return null; }
+    public E set(int index, E e) {
+
+        return null;
+    }
 
     // 지정한 위치의 요소를 제거하고, 제거한 값을 반환
     @Override
-    public E remove(int index) { return null; }
+    public E remove(int index) {
+        return null;
+    }
 
     // 주어진 객체와 같은 값을 갖는 첫 번째 요소의 인덱스를 반환 (없으면 -1)
     @Override
-    public int indexOf(Object o) { return -1; }
+    public int indexOf(Object o) {
+        return -1;
+    }
+
+
+    private void ensureCapacity() {
+        if (size == elementData.length) {
+            Object[] newData = new Object[elementData.length * 2];
+//            System.arraycopy(elementData, 0, newData, 0, size);
+            for (int i = 0; i < size; i++) {
+                newData[i] = elementData[i];
+            }
+            elementData = newData;
+        }
+    }
 }
